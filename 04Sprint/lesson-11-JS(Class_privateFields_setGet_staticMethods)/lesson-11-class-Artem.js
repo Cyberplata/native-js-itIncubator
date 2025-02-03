5955247247c7aae272bfac244c5e3e6e3b459c8b
PlatonKynin <kininplaton@gmail.com>
2025-02-03 23:02:09 +0300

    04-Sprint-js-week3-> 11-class остановился на геттерах и сеттерах пункт 7

--- 04Sprint/lesson-11-JS(Class_privateFields_setGet_staticMethods)/lesson-11-class-Artem.js
+++ 04Sprint/lesson-11-JS(Class_privateFields_setGet_staticMethods)/lesson-11-class-Artem.js
@@ -1,4 +1,5 @@
 // Function constructors, Class
+// https://www.youtube.com/watch?v=EDMMHcC7yUs
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 // Зачем нам нужны class? Создание однотипных объектов.
 
@@ -87,17 +88,12 @@
 // 2. Описываем функцию конструктор, которая принимает какие-то параметры.
 // 3. Мы можем не использовать конструктор, но тогда мы не сможем передать параметры для создания объектов.
 // 4. Получается мы обернули в обёртку наши функцию конструктора.
-class Car {
-    constructor(brand, color) {
-        this.brand = brand
-        this.color = color
-    }
-    startEngine() {
-        console.log(`${this.brand} engine is started`)
-    }
-}
+// 5. Приватные свойства через _ условная договорённость
+// 6. Приватные свойства через #. К приватному свойству можно обращаться только внутри самого класса.
+// 7. Мы не работаем с приватными методами напрямую, а через методы get и set.
+// Мы контролируем приватные свойства от создания до изменения.
 
-// // Равносильно вот такой записи ->
+// // Равносильно вот такой записи-псевдокод ->
 // class Car {
 //     function CarCreator(brand, color) {
 //         this.brand = brand
@@ -108,3 +104,46 @@ class Car {
 //         console.log(`${this.brand} engine is started`)
 //     }
 // }
+
+class Car {
+    #wheels = 4 // Объявление приватного свойства
+    constructor(brand, color, wheels) {
+        this.brand = brand
+        this.color = color
+        // this._wheels = wheels // условная договорённость приватных свойств
+        // this.#wheels = wheels
+    }
+
+    getWheels() { // Получение приватного свойства
+        return this.#wheels
+    }
+
+    setWheels(wheels) { // Установка приватного свойства
+        if (wheels > 10) throw new Error('wheels <= 10')
+        this.#wheels = wheels
+    }
+
+
+
+    startEngine() {
+        console.log(`${this.brand} engine is started`)
+    }
+}
+
+const car1 = new Car('bwm', 'red')
+const car2 = new Car('kia', 'black')
+
+console.log(car1)
+console.log(car2)
+console.log(car1.startEngine === car2.startEngine) // true
+car1.startEngine() // методы помещаются в объект прототипа класса по умолчанию
+car2.startEngine()
+
+// console.log(car1._wheels)
+// console.log(car1.#wheels)
+console.log(car1.getWheels()) // 4
+car1.setWheels(6)
+
+console.log(car1.getWheels()) // 6
+car1.setWheels(11) // Error wheels <= 10
+// Time: 53:20
